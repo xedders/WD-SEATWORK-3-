d2c789572cee119dd975ebb79e056ff824ffeab2
xedders <167269716+xedders@users.noreply.github.com>
2024-06-15 20:52:28 +0800

    test

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function openGallery(section) {
+    if (section === 'header') {
+        window.location.href = './header-gallery/index.html'; // Navigate to the index.html in header-gallery folder
+    } else if (section === 'footer') {
+        window.location.href = './footer-gallery/index.html'; // Navigate to the index.html in footer-gallery folder
+    }
+}
