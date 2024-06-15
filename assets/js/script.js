43597efc453fc10420d0d3127a19804578ba4626
xedders <167269716+xedders@users.noreply.github.com>
2024-06-15 23:01:17 +0800

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
-    <link rel="stylesheet" href="style.css"> <!-- Link to your CSS file -->
-</head>
-<body>
-
-<!-- Header Section -->
-<header class="header">
-    <div class="inner-header flex">
-        <svg version="1.1" class="logo" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
-             xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 500 500" xml:space="preserve">
-            <path fill="#FFFFFF" stroke="#000000" stroke-width="10" stroke-miterlimit="10" d="M57,283" />
-            <g>
-                <path fill="#fff"
-                      d="M250.4,0.8C112.7,0.8,1,112.4,1,250.2c0,137.7,111.7,249.4,249.4,249.4c137.7,0,249.4-111.7,249.4-249.4
-                          C499.8,112.4,388.1,0.8,250.4,0.8z M383.8,326.3c-62,0-101.4-14.1-117.6-46.3c-17.1-34.1-2.3-75.4,13
+<script>
+function openGallery(section) {
+    if (section === 'header') {
+        window.location.href = 'static-header-designs/header_static_1.html'; // Replace with the appropriate header design URL
+    } else if (section === 'footer') {
+        window.location.href = 'animated-header-designs/header_animated_1.html'; // Replace with the appropriate footer design URL
+    }
+}
+</script>
